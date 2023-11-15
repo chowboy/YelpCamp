92a7f5082e9fe425c32e6ef9a524d94219846257
chowboy <chris.f.chow@gmail.com>
2023-11-15 15:26:41 -0600

    updating dbURL for mongo connection

--- seeds/index.js
+++ seeds/index.js
@@ -1,14 +1,19 @@
+require('dotenv').config();
+
+
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+mongoose.connect(dbUrl);
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
-    useNewUrlParser: true,
-    // // useCreateIndex: true,
-    useUnifiedTopology: true
-});
+// mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
+//     useNewUrlParser: true,
+//     // // useCreateIndex: true,
+//     useUnifiedTopology: true
+// });
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
